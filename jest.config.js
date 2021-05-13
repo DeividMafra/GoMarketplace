7f2107addb44fb6c6bc2065560afade4e3d30782
DeividMafra <deividmafra@aol.com>
2021-05-12 23:06:40 -0400

    solved the problem

--- jest.config.js
+++ jest.config.js
@@ -2,18 +2,10 @@ module.exports = {
   preset: '@testing-library/react-native',
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  setupFiles: [
-    // './node_modules/react-native/Libraries/Animated/NativeAnimatedHelper',
-    './jest.setup.js',
-    // './src/setupTest.ts',
-  ],
+  setupFiles: ['./jest.setup.js'],
   moduleNameMapper: {
     '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/src/__mocks__/fileMock.ts',
-    // 'mock-modules.js': '<rootDir>/src/__mocks__/fileMock.js',
   },
-  // transform: {
-  //   '\\.(js|ts|tsx)$': require.resolve('react-native/jest/preprocessor.js'),
-  // },
   transformIgnorePatterns: ['node_modules/(?!@react-native|react-native)'],
 };
--- src/setupTest.ts
+++
@@ -1,3 +0,0 @@
-import mockAsyncStorage from '@react-native-community/async-storage/jest/async-storage-mock.js';
-
-jest.mock('@react-native-community/async-storage', () => mockAsyncStorage);
