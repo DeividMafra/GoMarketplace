b877f613ad0485e36d19e83d878a2e4ffc623442
DeividMafra <deividmafra@aol.com>
2021-05-12 22:54:27 -0400

    added node_modules because I'm getting crazy with this error

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,19 @@
-// module.exports = {
-//   preset: 'react-native',
-//   testEnvironment: 'node',
-//   setupFiles: ['./jest.setup.js'],
-//   moduleNameMapper: {
-//     '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-//       '<rootDir>/src/__mocks__/fileMock.ts',
-//   },
-// };
-
 module.exports = {
   preset: '@testing-library/react-native',
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  setupFiles: ['./jest.setup.js'],
+  setupFiles: [
+    // './node_modules/react-native/Libraries/Animated/NativeAnimatedHelper',
+    './jest.setup.js',
+    // './src/setupTest.ts',
+  ],
   moduleNameMapper: {
     '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/src/__mocks__/fileMock.ts',
+    // 'mock-modules.js': '<rootDir>/src/__mocks__/fileMock.js',
   },
+  // transform: {
+  //   '\\.(js|ts|tsx)$': require.resolve('react-native/jest/preprocessor.js'),
+  // },
   transformIgnorePatterns: ['node_modules/(?!@react-native|react-native)'],
 };
