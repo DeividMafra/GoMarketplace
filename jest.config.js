939329be2ef725ca1e8b17c23843693f0a6facf1
DeividMafra <deividmafra@aol.com>
2021-05-12 23:36:56 -0400

    patch changes

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,3 @@
-// module.exports = {
-//   preset: 'react-native',
-//   testEnvironment: 'node',
-//   setupFiles: ['./jest.setup.js'],
-//   moduleNameMapper: {
-//     '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-//       '<rootDir>/src/__mocks__/fileMock.ts',
-//   },
-// };
-
 module.exports = {
   preset: '@testing-library/react-native',
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
